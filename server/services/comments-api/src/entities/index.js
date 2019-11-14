3ebea9ff6d41a4404e8ce3cba877806c34d95238
Midhun Kandathil <pkr.midhun@gmail.com>
2019-11-14 09:39:25 +0530

    Added makeSource to index

--- server/services/comments-api/src/entities/index.js
+++ server/services/comments-api/src/entities/index.js
@@ -1,16 +1,24 @@
 import crypto from 'crypto';
 import sanitizeHTML from 'sanitize-html';
-import Id from '../../utils/Id'
+import ipRegex from 'ip-regex';
+import Id from '../utils/Id'
 import commentFactory from './comment.entity';
-
-const makeComment = commentFactory.makeComment({Id, md5, sanitize});
+import sourceFactory from './source.entity';
 
 const md5 = (text) => {
     return crypto.createHash('md5').update(text, 'utf-8').digest('hex');
 };
 
-const sanitize = () => {
+const sanitize = (text) => {
     return sanitizeHTML(text);
 }
+
+const isValidIp = (ip) => {
+    return ipRegex({ exact: true }).test(ip);
+};
+
+const makeSource = sourceFactory({isValidIp});
+
+const makeComment = commentFactory({Id, md5, sanitize, makeSource});
 
 export default makeComment;
