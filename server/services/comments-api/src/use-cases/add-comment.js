2179799e42568fe520fffd6723800ea512933ee3
Midhun Kandathil <pkr.midhun@gmail.com>
2019-11-15 23:37:24 +0530

    Added add-comments usecase

---
+++ server/services/comments-api/src/use-cases/add-comment.js
@@ -0,0 +1,31 @@
+import  makeComment from '../entities';
+
+const addCommentFactory = ({ commnetsDb }) => {
+    const addComment = async (commentInfo) => {
+        const comment = makeComment(commentInfo);
+        const exists = await commnetsDb.findByHash({ hash: comment.getHash() });
+        if (exists) {
+            return exists;
+        }
+        const commentSource = comment.getSource();
+        return commnetsDb.insertOne({
+            author: comment.getAuthor(),
+            createdOn: comment.getCreatedOn(),
+            hash: comment.gethash(),
+            id: comment.getId(),
+            modifiedOn: comment.getModifiedOn(),
+            postId: comment.getPostId(),
+            published: comment.isPublished(),
+            replyToId: comment.getReplyToId(),
+            source: {
+                ip: commentSource.getIp(),
+                browser: commentSource.getBrowser(),
+                referrer: commentSource.getReferrer()
+            },
+            text: comment.getText() 
+        });
+    };
+    return addComment;
+};
+
+export default addCommentFactory;
