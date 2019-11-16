d8bbfb85816c07b9a23d836e09d76415d4a7b3a0
Midhun Kandathil <pkr.midhun@gmail.com>
2019-11-16 20:02:25 +0530

    Added comment-db and its spec

---
+++ server/services/comments-api/src/adapters/data-access/comment-db.js
@@ -0,0 +1,67 @@
+import Id from '../../utils/Id';
+
+const makeCommentsDb = ({ makeDb }) => {
+    const collectionName = 'comments';
+    
+    const findAll = async ({ publishedOnly = true } = {}) => {
+        const db = await makeDb();
+        const query = publishedOnly ? { published: true } : {};
+        const result = await db.collection(collectionName).find(query);
+        return (await result.toArray()).map(({ _id: id, ...comment }) => ({
+            id,
+            ...comment
+        }));
+    };
+    
+    const findById = async ({ id: _id }) => {
+        const db = await makeDb();
+        const query = { _id };
+        const result = await db.collection(collectionName).findOne(query);
+        if (!result) {
+            return null;
+        }
+        const { _id: id, ...info } = result;
+        return { id, ...info };
+    };
+
+    const findByHash = async ({ hash }) => {
+        const db = await makeDb();
+        const query = { hash };
+        const result = await db.collection(collectionName).findOne(query);
+        if (!result) {
+            return null;
+        }
+        const { _id: id, ...info } = result;
+        return { id, ...info };
+    };
+
+    const insert = async ({ id: _id = Id.makeId(), ...commentInfo }) => {
+        const db = await makeDb();
+        const result = await db.collection(collectionName).insertOne({ _id, ...commentInfo });
+        const { _id: id, ...insertedInfo } = result.ops[0];
+        return { id, ...insertedInfo };
+    };
+
+    const update = async ({ id: _id, ...commentInfo }) => {
+        const db = await makeDb();
+        const result = await db.collection(collectionName).updateOne({ _id }, { $set: { ...commentInfo } });
+        return result.modifiedCount > 0 ? { id: _id, ...commentInfo }: null;
+    };
+
+    const remove = async ({ id: _id }) => {
+        const db = await makeDb();
+        const result = await db.collection(collectionName).deleteOne({ _id });
+        return result.deletedCount;
+    };
+
+    return Object.freeze({
+        findAll,
+        findById,
+        findByHash,
+        insert,
+        update,
+        remove
+    });
+};
+
+export default makeCommentsDb;
---
+++ server/services/comments-api/src/adapters/data-access/comment.db.spec.js
@@ -0,0 +1,76 @@
+import { expect } from 'chai';
+import makeDb, { client } from '../../utils/fixtures/db';
+import makeCommentsDb from './comment-db';
+import makeFakeComment from '../../utils/fixtures/comment';
+
+describe('comments db', () => {
+    let commentsDb;
+
+    before(async () => {
+        await makeDb();
+    });
+  
+    beforeEach(() => {
+      commentsDb = makeCommentsDb({ makeDb });
+    });
+
+    it('lists comments', async () => {
+        const inserts = await Promise.all(
+          [ makeFakeComment(), makeFakeComment(), makeFakeComment() ].map(
+            commentsDb.insert
+          )
+        );
+        const commentsFound = await commentsDb.findAll();
+        expect(inserts.length).to.equal(3);
+        inserts.forEach(insert => expect(commentsFound).to.deep.include(insert));
+    });
+
+    it('finds a comment by id', async () => {
+        const comment = makeFakeComment();
+        let found = await commentsDb.findById(comment);
+        expect(found).to.be.null;
+        await commentsDb.insert(comment);
+        found = await commentsDb.findById(comment);
+        expect(found).to.eql(comment);
+    });
+
+    it('finds a comment by hash', async () => {
+        const comment = makeFakeComment();
+        let found = await commentsDb.findByHash(comment);
+        expect(found).to.be.null;
+        await commentsDb.insert(comment);
+        found = await commentsDb.findByHash(comment);
+        expect(found).to.eql(comment);
+    });
+
+    it('inserts a comment', async() => {
+        const comment = makeFakeComment();
+        const result = await commentsDb.insert(comment);
+        expect(result).to.eql(comment); 
+    });
+
+    it('updates a comment', async() => {
+        const comment = makeFakeComment();
+        let result = await commentsDb.update(comment);
+        expect(result).to.be.null;
+        await commentsDb.insert(comment);
+        comment.text = 'changed';
+        result = await commentsDb.update(comment);
+        expect(result).to.eql(comment);
+    });
+
+    it('deletes a comment', async() => {
+        const comment = makeFakeComment();
+        let result = await commentsDb.remove(comment);
+        expect(result).to.eql(0);
+        await commentsDb.insert(comment);
+        result = await commentsDb.remove(comment);
+        expect(result).to.eql(1);
+    });
+
+    after(async () => {
+        const db = await makeDb();
+        await db.dropDatabase();
+        await client.close();
+    });
+});
