89e798d841f4e7db26c5c9ddef6a5531b672762e
Midhun Kandathil <pkr.midhun@gmail.com>
2019-11-16 20:01:46 +0530

    Added make-db for testing

---
+++ server/services/comments-api/config/make-db.js
@@ -0,0 +1,20 @@
+/**
+ * Module dependencies
+ */
+import mongodb from 'mongodb';
+import config from './config';
+
+const MongoClient = mongodb.MongoClient;
+const client = new MongoClient(config.db.uri, config.db.options);
+
+/**
+ * Connect to mongodb
+ */
+const makeDb = async () => {
+	if (!client.isConnected()) {
+		await client.connect();
+	}
+	return client.db(config.db.name);
+};
+
+export default makeDb;
