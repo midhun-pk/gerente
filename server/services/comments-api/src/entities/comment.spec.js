2ec0bf8b0635c86bbc4136b001b42b9b0a05347f
Midhun Kandathil <pkr.midhun@gmail.com>
2019-11-16 20:04:44 +0530

    formatted comment spec

--- server/services/comments-api/src/entities/comment.spec.js
+++ server/services/comments-api/src/entities/comment.spec.js
@@ -6,17 +6,17 @@ describe('comment', () => {
     it('must have an author', () => {
         const comment = makeFakeComment({ author: null });
         expect(() => makeComment(comment)).to.throw('Comment must have an author.');
-    })
+    });
     it('must have a valid post id', () => {
         const comment = makeFakeComment({ postId: null });
         expect(() => makeComment(comment)).to.throw('Comment must contain a postId.');
-    })
+    });
     it('must have valid text', () => {
         const comment = makeFakeComment({ text: null });
         expect(() => makeComment(comment)).to.throw(
           'Comment must include at least one character of text.'
         );
-    })
+    });
     it('can be in reply to another comment', () => {
         const comment = makeFakeComment({ replyToId: 'invalid' });
         expect(() => makeComment(comment)).to.throw(
@@ -24,46 +24,46 @@ describe('comment', () => {
         );
         const notInReply = makeFakeComment({ replyToId: undefined });
         expect(() => makeComment(notInReply)).not.to.throw();
-    })
+    });
     it('can have an id', () => {
         const comment = makeFakeComment({ id: 'invalid' });
         expect(() => makeComment(comment)).to.throw('Comment must have a valid id.');
         const noId = makeFakeComment({ id: undefined });
         expect(() => makeComment(noId)).not.to.throw();
-    })
+    });
     it('can create an id', () => {
         const noId = makeFakeComment({ id: undefined });
         const comment = makeComment(noId);
         expect(comment.getId()).not.to.be.undefined;
-    })
+    });
     it('can be published', () => {
         const unpublished = makeFakeComment({ published: false });
         const comment = makeComment(unpublished);
         expect(comment.isPublished()).to.be.false;
-        comment.publish()
+        comment.publish();
         expect(comment.isPublished()).to.be.true;
-    })
+    });
     it('can be unpublished', () => {
         const published = makeFakeComment({ published: true });
         const comment = makeComment(published);
         expect(comment.isPublished()).to.be.true;
-        comment.unPublish()
+        comment.unPublish();
         expect(comment.isPublished()).to.be.false;
-    })
+    });
     it('is createdOn now in UTC', () => {
         const noCreationDate = makeFakeComment({ createdOn: undefined });
         expect(noCreationDate.createdOn).to.be.undefined;
         const date = makeComment(noCreationDate).getCreatedOn();
         expect(date).not.to.be.undefined;
         expect(new Date(date).toUTCString().substring(26)).to.be.string('GMT');
-    })
+    });
     it('is modifiedOn now in UTC', () => {
         const noModifiedOnDate = makeFakeComment({ modifiedOn: undefined });
         expect(noModifiedOnDate.modifiedOn).to.be.undefined;
         const date = makeComment(noModifiedOnDate).getCreatedOn();
         expect(date).not.to.be.undefined;
         expect(new Date(date).toUTCString().substring(26)).to.be.string('GMT');
-    })
+    });
     it('sanitizes its text', () => {
         const sane = makeComment({
           ...makeFakeComment({ text: '<p>This is fine</p>' })
@@ -82,7 +82,7 @@ describe('comment', () => {
         expect(() => makeComment(totallyInsane)).to.throw(
           'Comment contains no usable text.'
         );
-    })
+    });
     it('can be marked deleted', () => {
         const fake = makeFakeComment();
         const c = makeComment(fake);
@@ -90,11 +90,11 @@ describe('comment', () => {
         expect(c.isDeleted()).to.be.true;
         expect(c.getText()).to.be.string('.xX This comment has been deleted Xx.');
         expect(c.getAuthor()).to.be.string('deleted');
-    })
+    });
     it('includes a hash', () => {
         const fakeComment = {
           author: 'Bruce Wayne',
-          text: "I'm batman.",
+          text: 'I\'m batman.',
           postId: 'cjt65art5350vy000hm1rp3s9',
           published: true,
           source: { ip: '127.0.0.1' }
@@ -102,35 +102,35 @@ describe('comment', () => {
         expect(makeComment(fakeComment).getHash()).to.be.string(
           '7bb94f070d9305976b5381b7d3e8ad8a'
         );
-    })
+    });
     it('must have a source', () => {
         const noSource = makeFakeComment({ source: undefined });
         expect(() => makeComment(noSource)).to.throw('Comment must have a source.');
-    })
+    });
     it('must have a source ip', () => {
         const noIp = makeFakeComment({ source: { ip: undefined } });
         expect(() => makeComment(noIp)).to.throw(
           'Comment source must contain an IP.'
         );
-    })
+    });
     it('can have a source browser', () => {
-        const withBrowser = makeFakeComment()
+        const withBrowser = makeFakeComment();
         expect(
           makeComment(withBrowser)
             .getSource()
             .getBrowser()
-        ).to.be.string(withBrowser.source.browser)
-        const withoutBrowser = makeFakeComment({source: { ip: '127.0.0.1' }})
+        ).to.be.string(withBrowser.source.browser);
+        const withoutBrowser = makeFakeComment({ source: { ip: '127.0.0.1' } });
         expect(() => makeComment(withoutBrowser).getSource().getBrowser()).not.to.throw();
-    })
+    });
     it('can have a source referrer', () => {
-        const withReferrer = makeFakeComment()
+        const withReferrer = makeFakeComment();
         expect(
           makeComment(withReferrer)
             .getSource()
             .getReferrer()
-        ).to.be.string(withReferrer.source.referrer)
-        const withoutReferrer = makeFakeComment({source: { ip: '127.0.0.1' }})
+        ).to.be.string(withReferrer.source.referrer);
+        const withoutReferrer = makeFakeComment({ source: { ip: '127.0.0.1' } });
         expect(() => makeComment(withoutReferrer).getSource().getReferrer()).not.to.throw();
-    })
+    });
 });
